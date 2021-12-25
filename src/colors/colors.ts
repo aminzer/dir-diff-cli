2e48ac2c42da3c402eb14c61b02b9693fe4c1c8c
aminzer <minzer.alexey@gmail.com>
2021-12-25 22:17:54 +0300

    update color schema

--- src/colors/colors.ts
+++ src/colors/colors.ts
@@ -1,5 +1,5 @@
 import * as chalk from 'chalk';
 
-export const sourceOnly = chalk.blue;
+export const sourceOnly = chalk.green;
 export const targetOnly = chalk.red;
-export const different = chalk.yellow;
+export const different = chalk.blue;
